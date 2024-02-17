2d7ac8cb13cadcfea409192414ac27300f8cdacb
Redacks <redacks@web.de>
2024-02-17 22:15:13 +0100

    WIP moving to astro latests and implementing i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
-// https://astro.build/config
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-import sitemap from "astro-sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), sitemap()],
-  site: "https://www.redacks.com"
+  integrations: [tailwind(), sitemap(), robotsTxt(), astroI18next()]
 });
---
+++ working.md
@@ -0,0 +1,2 @@
+https://github.com/yassinedoghri/astro-i18next
+https://docs.astro.build/en/recipes/i18n/#translate-routes
