0ac77850630f44e1006ac70fe0e26c728d262cb4
RED-TUF\Hochschule <admin@redacks.com>
2024-03-20 15:16:03 +0100

    Adding DaisyUI and implementing basic Navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import astroI18next from "astro-i18next";
-
 import robotsTxt from "astro-robots-txt";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), astroI18next()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), astroI18next(), icon()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+	extend: {
+		fontFamily: {
+			default: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+			grotesque: ["Darker Grotesque", "sans-serif"],
+			inter: ["Inter Variable", "sans-serif"],
+		},
+	}
+  },
+  daisyui: {
+	themes: [
+		{
+		  mytheme: {
+			primary: "#00dbd8",
+			secondary: "#b133ff",
+			accent: "#ffffff",
+			neutral: "#0e1624",
+			"base-100": "#0e1624",
+		  },
+		},
+	  ],
+  },
+  plugins: [daisyui],
+};
